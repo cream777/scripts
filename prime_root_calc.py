486953639f8f5a0675cfa2f933bd794aba327380
max <86410229+cream777@users.noreply.github.com>
2021-09-28 18:45:18 +0100

    Prime check.
    Now detects if input was not prime.

--- prime_root_calc.py
+++ prime_root_calc.py
@@ -1,19 +1,29 @@
 while True:
     while True:
         try:
-            prime = int(input("Please enter a prime number: "))
+            prime = int(input("\nPlease enter a prime number: "))
         except ValueError:
             print("Please enter a valid integer\n")
-            pass
+            continue        
+        divs = 0
+        for i in range(1, prime):
+            if (prime / i) - (int(prime / i)) == 0:
+        
+                divs += 1
+        print(divs)
+        if divs > 1:
+            continue         
         else:
             break
     print("Prime roots for {}:\n".format(prime))
+    proots = []
     for i in range(1, prime):
         list =[]
         for n in range(1, prime):
             list.append((i ** n % prime))
         if len(set(list)) == len(list):
-            print(i)
+            proots.append(i)
+    print(proots)
     another = input("\nAnother? ")
     if another == "y" or another == "yes":
         pass
