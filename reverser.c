e8598bb440f98c4acc4a54d294ce8007b9d30e38
max <86410229+cream777@users.noreply.github.com>
2021-10-28 17:08:14 +0100

    Update reverser.c

--- reverser.c
+++ reverser.c
@@ -16,9 +16,14 @@ char* reverser(char* string, char* stringrev){
 
 int main(){
     char string[100];
-    char stringrev[strlen(string)];
+    char stringrev[100];
     printf("\nEnter string: ");
     fgets(string, 100, stdin);
+    int c;
+    if ( ! strchr(string, '\n')){
+        while ((c = getchar()) != EOF && c != '\n' ){};
+        
+    }
     printf("%s \n\n", reverser(string, stringrev));
     return 0;
 }
